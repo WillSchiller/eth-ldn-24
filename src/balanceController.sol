7b9b1fb0095bebd5ba96402dc3a69ace946d455d
Will Schiller <will.schiller@protonmail.com>
2024-03-16 22:22:19 +0000

    finish contructor

--- src/balanceController.sol
+++ src/balanceController.sol
@@ -1,27 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-//todo import zeppelin ownbale
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 
-contract BalanceController {
+contract BalanceController  is Ownable{
 
     struct TargetBalance {
-        uint256 target;
-        uint256 lowerBound;
+        uint256 target; // the target wallet balance
+        uint256 buffer; // the amount over or under the target balance that is acceptable
+        uint256 lowerBound; 
         uint256 upperBound;
     }
-
     TargetBalance public targetBalance;
+    address public delayModuleAddress;
 
-    error targetTopupOutsideOfRange()
-
-
-    constructor(uint256 _initialTargetBalance) {
-        targetBalance = _initialTargetBalance;
+    error targetTopupOutsideOfRange();
+    constructor(uint256 _initialTargetBalance, address _delayModuleAddress) {
+        owner = msg.sender;
+        setTargetBalance(_initialTargetBalance, 2);
+        delayModuleAddress = _delayModuleAddress;
     }
 
-    // todo add function for setting target balance 
-
+    function setTargetBalance(uint256 _targetBalance, uint256 buffer) public onlyOwner {
+        uint256 bufferAmount = (_targetBalance * buffer) / 100;
+        targetBalance.target = _targetBalance;
+        targetBalance.buffer = buffer;
+        targetBalance.lowerBound = _targetBalance - bufferAmount;
+        targetBalance.upperBound = _targetBalance + bufferAmount;
+    }
 
     function topUpBalance(uint256 _amoun) public { //amount
         // amount + balance of this contract must target balance within 3%
