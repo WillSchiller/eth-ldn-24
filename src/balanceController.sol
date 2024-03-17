f3959606c97bc0f082acac48d5cea98b5282ba68
Will Schiller <will.schiller@protonmail.com>
2024-03-17 01:16:33 +0000

    install zodiac

--- src/balanceController.sol
+++ src/balanceController.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.20;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {Delay} from '@zodiac/contracts/Delay.sol';
 
 contract BalanceController is Ownable(msg.sender){
     struct TargetBalance {
